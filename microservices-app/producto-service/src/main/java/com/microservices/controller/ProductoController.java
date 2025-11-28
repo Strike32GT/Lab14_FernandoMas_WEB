0ee18b31be97f1117a02c3bb95120b8a44cf4916
Strike32GT <roberto.mas@tecsup.edu.pe>
2025-11-27 19:36:17 -0500

    Agregando expansion para que lo lea tambien en la nube mi api de productos

--- microservices-app/producto-service/src/main/java/com/microservices/controller/ProductoController.java
+++ microservices-app/producto-service/src/main/java/com/microservices/controller/ProductoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "lab14_fernandomas_web.railway.internal"})
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
